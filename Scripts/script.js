8978bd70157be0cb071c7bd1405d50940feba939
Michael-B7 <mbrashears2006@gmail.com>
2023-05-09 14:18:33 -0700

    started to implement iframes to show websites

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,15 @@
+class PreviewModal{
+    constructor(link){
+        let navWidth = document.getElementsByTagName("nav")[0];
+        console.log(navWidth.)
+        this.link = link
+        this.html = 
+        `<div class="preview-modal" style="position:fixed; z-index:100; width:calc(100vw-${navWidth}); height:90%;">
+            <iframe src="${this.link}" frameborder="8" style="width:100%; height:100%;"></iframe>
+        </div>`   
+    }
+}
+
 let submenu = document.getElementsByClassName("submenu")[0];
 let pSubmenu = submenu.parentElement;
 let nav = pSubmenu.parentElement;
@@ -37,8 +49,15 @@ function animateCarousel() {
 }  
 
 if(window.location.pathname === "/portfolio/personal.html"){
+    webPreview("https://mbzg.netlify.app/");
     carousel = document.getElementById("carousel-container");
     carousel.style.width = document.getElementsByClassName("python")[0].width;
 
     animateCarousel();
+}
+
+function webPreview(link){
+    let modal = new PreviewModal(link);
+    let article = document.getElementsByTagName("body")[0];
+    article.innerHTML += modal.html;
 }
